df4ef33c53697a0ff1ad5bbfe8154accbd112896
RashmikantaPanda <rashmikantapanda1234@gmail.com>
2022-09-28 19:29:16 +0530

    Updated

--- src/my/basic/programs/FindASCII.java
+++ src/my/basic/programs/FindASCII.java
@@ -3,7 +3,7 @@ package my.basic.programs;
 import java.util.Scanner;
 
 public class FindASCII {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a character : ");
         char ch1=sc.next().charAt(0);
